5cb9bd57a91a3bfc26fe868f0ba9a920a9129a65
Thiago <thiago_murakami@hotmail.com>
2023-09-13 10:10:05 -0300

    teste automatizado com pytest

---
+++ PythonTDD/readme.md
@@ -0,0 +1,3 @@
+### Pytest
+- Executar o comando *pytest* no terminal. No retorno vai teremos *teste\test_bytebank.py .*
+- Outra alternativa é acrescentar *pytest -v*, de verbose. Receberemos um retorno mais detalhado
---
+++ PythonTDD/teste/test_bytebank.py
@@ -0,0 +1,13 @@
+from bytebank import Funcionario
+
+class TestCalss:
+    """criando class de teste"""
+    def test_quando_idade_recebe_01_01_2000_retorna_23(self):
+        """teste quando idade for igual a 23"""
+        entrada = "01/01/2000"
+        esperado = 23
+
+        funcionario_teste = Funcionario("Thiago", entrada, 1000)
+        resulta = funcionario_teste.idade()
+        # verifica se o que estou passando 'e verdade ou nao
+        assert resulta == esperado
